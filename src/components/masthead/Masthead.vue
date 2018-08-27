9af7de11005d7ae48ef4ee5d6fc2804a41feb44e
Joseph Jin <jinyuhui61@hotmail.com>
2018-08-27 12:29:11 -0700

    banner and search bar

---
+++ src/components/masthead/Masthead.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="banner">
+    <form action="get">
+      <el-row>
+        <el-col :span="4">
+          <el-select v-model="value" placeholder="找工作">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-col>
+        <el-col :span="14">
+          <el-input
+            class-name="keywords"
+            placeholder="输入关键字搜索"
+            v-model="keywords"
+            clearable>
+          </el-input>
+        </el-col>
+        <el-col :span="6">
+          <el-button native-type='submit'>搜索</el-button>
+        </el-col>
+      </el-row>
+    </form>
+  </div>
+</template>
+
+<script>
+import { Row, Select, Option, Input, Col, Button } from 'element-ui'
+import Vue from 'vue'
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Input)
+Vue.use(Row)
+Vue.use(Button)
+Vue.use(Col)
+
+export default {
+  name: 'Masthead',
+  data() {
+    return {
+      options: [
+        {
+          value: 'jobs',
+          label: '找工作'
+        },
+        {
+          value: 'hire',
+          label: '招聘'
+        }
+      ],
+      value: '找工作',
+      keywords: ''
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.banner /deep/ .el-button {
+  width: 100%;
+  height: 55px;
+  padding-left: 50px;
+  border: none;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  background-color: #303a40;
+  position: relative;
+  color: #ffffff;
+  font-size: 20px;
+  &::before {
+    content: '';
+    position: absolute;
+    top: 12.5px;
+    left: 38px;
+    display: block;
+    height: 30px;
+    width: 30px;
+    background-image: url('../../assets/images/sprite.png');
+    background-position: -67px 0;
+  }
+}
+.banner /deep/ .el-input__inner {
+  height: 55px;
+  outline: none;
+  border: none;
+  color: #999;
+  &[class-name="keywords"] {
+    border-left: 2px solid #303a40;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+}
+.banner {
+  height: 500px;
+  background-image: url('../../assets/images/banner3.jpg');
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  form {
+    background-color: #fff;
+    width: 670px;
+    margin-bottom: 20px;
+    border-radius: 5px;
+    .el-button {
+      img {
+        height: 30px;
+        width: 30px;
+      }
+    }
+  }
+}
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,41 +1,7 @@
 <template>
   <div>
     <holder></holder>
-    <div>
-      {{this.msg}} <br/>
-      <br/>
-      <router-link to="/login">
-        <el-button type="primary" round>点击跳转到登录login.vue页面，去开发吧，加油哦！</el-button>
-      </router-link>
-      <br/><br/>
-      <router-link to="/feedback">
-        <el-button type="warning" round>点击跳转到反馈feedback.vue页面，去开发吧，加油哦！</el-button>
-      </router-link>
-      <br/><br/>
-      <router-link to="/postdetail">
-        <el-button type="info" round>点击跳转到职位详情postDetail.vue页面，去开发吧，加油哦！</el-button>
-      </router-link>
-      <br/><br/>
-      <router-link to="/findpw">
-        <el-button type="success" round>点击跳转到找回密码findpw.vue页面，去开发吧，加油哦！</el-button>
-      </router-link>
-      <br/><br/>
-      <router-link to="/register">
-        <el-button type="info" round>点击跳转到注册register.vue页面，去开发吧，加油哦！</el-button>
-      </router-link>
-      <br/><br/>
-      <router-link to="/createresume">
-        <el-button type="warning" round>点击跳转到创建简历createResume.vue页面，去开发吧，加油哦！</el-button>
-      </router-link>
-      <br/><br/>
-      <router-link to="/selfcenter">
-        <el-button type="danger" round>点击跳转到个人中心selfCenter.vue页面，去开发吧，加油哦！</el-button>
-      </router-link>
-      <br/> <br/>
-      <router-link to="/about">
-        <el-button type="primary" round>点击跳转到关于about.vue页面，去开发吧，加油哦！</el-button>
-      </router-link>
-    </div>
+    <Masthead />
     <foot></foot>
   </div>
 </template>
@@ -43,6 +9,7 @@
 import Vue from 'vue'
 import holder from '@/components/header/header'
 import foot from '@/components/footer/footer'
+import Masthead from '@/components/masthead/Masthead'
 
 import { Button } from 'element-ui'
 Vue.use(Button)
@@ -51,9 +18,10 @@ export default {
   name: 'home',
   components: {
     foot,
-    holder
+    holder,
+    Masthead
   },
-  data () {
+  data() {
     return {
       msg: '当前是首页页面，首页基于当前进行开发即可'
     }
