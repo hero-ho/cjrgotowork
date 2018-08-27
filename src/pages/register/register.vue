6016e6bcc0f1d8652e274e2f1407cfc6673596af
hero-ho <hero_ho@sina.com>
2018-08-27 10:48:25 +0800

    项目初始化

---
+++ src/pages/register/register.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{this.msg}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'register',
+  data () {
+    return {
+      msg: '当前是注册页面'
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
