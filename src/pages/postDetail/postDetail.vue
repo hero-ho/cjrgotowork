104bc1f679b324f17730db4c9d566a5d5546cd89
fangy <282735445@qq.com>
2018-08-29 09:39:16 +0800

    robert-update

--- src/pages/postDetail/postDetail.vue
+++ src/pages/postDetail/postDetail.vue
@@ -11,12 +11,25 @@
           </div>
           <div class="job-name-right">
             <el-button class="resume">投递简历</el-button>
-            <div class=""></div>
+            <div class="function-btn">
+              <el-button type="text" icon="el-icon-star-off">收藏</el-button>
+              <el-button type="text" icon="el-icon-warning">报错</el-button>
+            </div>
           </div>
         </div>
       </div>
       <div class="job-detail-body">
-
+        <div class="detail-left-wrap">
+          <div class="detail">
+            <div class="detail-info">
+              <div class=""></div>
+            </div>
+          </div>
+          <div class="other-job">123</div>
+        </div>
+        <div class="detail-right-wrap">
+          <div class="hot-jobs">123</div>
+        </div>
       </div>
     </div>
     <foot></foot>
@@ -44,56 +57,91 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-  .job-detail-header {
-    background-color: #303a40;
-    .job-name-detail {
-      width: 1200px;
-      height: 170px;
-      margin: 0 auto;
-      overflow: hidden;
-      .job-name-left {
-        width: 50%;
-        float: left;
-        .release-time {
-          padding-top: 50px;
-          color: #cccccc;
-          font-size: 12px
-        }
-        .job-name {
-          margin-top: 17px;
-          color: #ffffff;
-          line-height: 24px;
-          .name {
-            vertical-align: top;
-            font-size: 24px;
+  .job-detail-wrap {
+    background-color: rgb(238, 238, 238);
+    .job-detail-header {
+      background-color: #303a40;
+      .job-name-detail {
+        width: 1200px;
+        height: 170px;
+        margin: 0 auto;
+        overflow: hidden;
+        .job-name-left {
+          width: 50%;
+          float: left;
+          .release-time {
+            padding-top: 50px;
+            color: #cccccc;
+            font-size: 12px
           }
-          .salary {
-            vertical-align: middle;
-            margin-left: 10px;
-            border-radius: 10px;
-            background-color: #f46d43;
+          .job-name {
+            margin-top: 17px;
+            color: #ffffff;
+            line-height: 24px;
+            .name {
+              vertical-align: top;
+              font-size: 24px;
+            }
+            .salary {
+              vertical-align: middle;
+              margin-left: 10px;
+              border-radius: 10px;
+              background-color: #f46d43;
+              font-size: 14px;
+              padding: 0 10px
+            }
+          }
+          .company-name {
+            margin-top: 20px;
+            color: #ffffff;
+            font-size: 14px
+          }
+        }
+        .job-name-right {
+          text-align: right;
+          width: 50%;
+          float: right;
+          .resume {
+            margin-top: 70px;
+            width: 150px;
+            height: 40px;
+            background-color: #ff9d00;
+            color: #ffffff;
             font-size: 14px;
-            padding: 0 10px
+            border-color: #ff9d00
           }
-        }
-        .company-name {
-          margin-top: 20px;
-          color: #ffffff;
-          font-size: 14px
+          .function-btn{
+            button {
+              width: 75px;
+              margin: 0;
+              color: white
+            }
+          }
         }
       }
-      .job-name-right {
-        text-align: right;
-        width: 50%;
-        float: right;
-        .resume {
-          margin-top: 70px;
-          width: 150px;
-          height: 40px;
-          background-color: #ff9d00;
-          color: #ffffff;
-          font-size: 14px;
-          border-color: #ff9d00
+    }
+    .job-detail-body {
+      width: 1200px;
+      margin: 0 auto;
+      padding-top: 10px;
+      overflow: hidden;
+      .detail-left-wrap {
+        width: 800px;
+        float: left;
+        .detail {
+          background-color: white;
+        }
+        .other-job {
+          margin-top: 10px;
+          background-color: white;
+        }
+      }
+      .detail-right-wrap {
+        width: 380px;
+        float: left;
+        padding-left: 20px;
+        .hot-jobs {
+          background-color: white;
         }
       }
     }
